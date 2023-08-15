27d9770015926acbeae15206cb5519372cc1ac79
Laraib Kamal Khan <khan1999laraib@gmail.com>
2023-08-15 22:08:17 +0530

    oops topics added

---
+++ libraryanagement.java
@@ -0,0 +1,15 @@
+class Library{
+    Library(){
+        System.out.println("Welcome to Library");
+    }
+   
+}
+
+public class libraryanagement {
+    public static void main(String[] args) {
+        
+        Library L1 = new Library();
+
+        
+    }
+}
