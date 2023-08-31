5bdf6daea4e32c792f3b7e709b37bad117756647
Laraib Kamal Khan <khan1999laraib@gmail.com>
2023-08-31 21:24:58 +0530

    inheri in interfaces

---
+++ prac1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class prac1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+       int a = 10;
+       int b = 20 ; 
+       if(a==b){
+        System.out.println("a=b");
+       }
+       else if(a>b){
+        System.out.println("a>b");
+        
+       }
+       
+       else if(a<b){
+        System.out.println("a<b");
+       }
+       
+
+    }
+    
+}
